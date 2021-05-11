ad797d5e1621e3ea4850e87d4c2c3842a1ca7277
Corentin Girard <corentingirard.dev@gmail.com>
2021-05-12 01:31:27 +0200

    Improvements

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const StegCloak = require('stegcloak');
 
-console.log('Loading reveal.js');
+const password = 'pass';
+const revealedIdentifier = '<strong>[Revealed]</strong>';
 
 const containsInvisible = text => text.match(RegExp(StegCloak.zwc.join('|')));
+const isRevealed = text => text.indexOf(revealedIdentifier) !== -1;
+const hide = text => `<s>${text}</s>`;
+const reveal = text => stegCloak.reveal(text, password);
+
+console.log('Loading reveal.js');
 
 const stegCloak = new StegCloak(true, false);
-const messages = Array.from(document.querySelectorAll("div[class*='messageContent-']")).filter(el => containsInvisible(el.innerText));
-const revealed = messages.map(el => `${el.innerText} --> ${stegCloak.reveal(el.innerText, 'pass')}`);
+const elements = Array.from(document.querySelectorAll("div[class*='messageContent-']"));
+const encrypted = elements.filter(el => containsInvisible(el.innerText) && !isRevealed(el.innerHTML));
 
-console.log({ revealed });
+encrypted.forEach(el => el.innerHTML = `${revealedIdentifier} ${hide(el.innerText)} ${reveal(el.innerText)}`);
+
+if (encrypted.length) {
+    alert(`${encrypted.length} messages were revealed!`);
+} else {
+    alert('No message to reveal.');
+}
--- reveal.js
+++ reveal.js
@@ -1,15 +1,27 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const StegCloak = require('stegcloak');
 
-console.log('Loading reveal.js');
+const password = 'pass';
+const revealedIdentifier = '<strong>[Revealed]</strong>';
 
 const containsInvisible = text => text.match(RegExp(StegCloak.zwc.join('|')));
+const isRevealed = text => text.indexOf(revealedIdentifier) !== -1;
+const hide = text => `<s>${text}</s>`;
+const reveal = text => stegCloak.reveal(text, password);
+
+console.log('Loading reveal.js');
 
 const stegCloak = new StegCloak(true, false);
-const messages = Array.from(document.querySelectorAll("div[class*='messageContent-']")).filter(el => containsInvisible(el.innerText));
-const revealed = messages.map(el => `${el.innerText} --> ${stegCloak.reveal(el.innerText, 'pass')}`);
+const elements = Array.from(document.querySelectorAll("div[class*='messageContent-']"));
+const encrypted = elements.filter(el => containsInvisible(el.innerText) && !isRevealed(el.innerHTML));
 
-console.log({ revealed });
+encrypted.forEach(el => el.innerHTML = `${revealedIdentifier} ${hide(el.innerText)} ${reveal(el.innerText)}`);
+
+if (encrypted.length) {
+    alert(`${encrypted.length} messages were revealed!`);
+} else {
+    alert('No message to reveal.');
+}
 },{"stegcloak":413}],2:[function(require,module,exports){
 'use strict'
 
